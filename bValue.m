466394e33a46990dc0f54134246a80dedd1196b3
Pedro Montanari <pedromontanari@vivaldi.net>
2023-12-21 19:52:31 +0100

    Minor changes + created example
    Minor changes in code organization. Created an example file.

--- bValue.m
+++ bValue.m
@@ -1,116 +1,121 @@
 function b_value = bValue(T,args)
 %bValue : Calculates the b-value and associated parameters
-%   Updated in 06/20/2023
 %   This function calculates the b-value for a set of data.
 %   Input arguments:
 %       T : (structure) containing the following field:
 %           1.  Time (double) = array containing signal times
+%           2.  Magnitude(double) = array containing the signal Magnitude
+%           in dB
 %       args = structure containing the following fields:
-%           1. MagRange (double) : scalar containing the difference between
-%           maximum and minimum
-%           signal magnitude
-%           2. Magnitude (double) : array containing signal magnitude
-%           3. bValueNumOfPointsInWindow (double) : scalar indicating how
+%           1. bValueNumOfPointsInWindow (double) : scalar indicating how
 %           many values to use in each b-value calculation
-%           4. bValueNumOfPointsToShiftInWindow (double) : scalar
+%           2. bValueNumOfPointsToShiftInWindow (double) : scalar
 %           indicating how many points to move each window.
 %           The calculation uses a sliding window scheme, which means
 %           overlap can be taken into account. For no overlap specify
-%           "bValueNumOfPointsToShiftInWindow" =
-%           "bValueNumOfPointsInWindow"
+%           'bValueNumOfPointsToShiftInWindow' =
+%           'bValueNumOfPointsInWindow'
+%           3.bValueMagnitudeIntervalSize (double): scalar indicating the
+%           interval size of magnitudes. By default use 0.1.
 %   Output arguments:
 %       b_value : (structure) with the following fields:
-%           1. Value : b_value itself
-%           2. Magnitude : Magnitude of signals (optional)
-%           3. NofHitsInEachInterval : Number of AE hits in each magnitude
-%           interval defined (optional)
-%           4. MagnitudeOfCompleteness : Magnitude of Completeness of the
-%           signals (see 10.1016/j.chaos.2015.09.004 for info) (optional)
-%           5. PositionOfMagnitudeOfCompleteness : Position of the
-%           magnitude which is the magnitude of completeness (optional)
+%           1. Input : (structure) with the following fields
+%               i.  Time: (double) array of the time values of the 
+%                   original signal
+%               ii. Signal: (double) array of the original input signal
+%               iii. Magnitude: (double) array of the magnitudes of the
+%               original signal
+%           2. Output: (structure) with the following fields
+%               i. Value : b_value itself
+%               ii.Time: time array of the b_values (obs.: not a "real"
+%               time, it is the time in the middle of each window.
+%           3. Aux: (structure) with the following fiels
+%               i. NofHitsInEachInterval : Number of AE hits in each magnitude
+%           interval defined
+%               ii. MagnitudeOfCompleteness : Magnitude of Completeness of the
+%           signals (see 10.1016/j.chaos.2015.09.004 for info)
+%               iii. PositionOfMagnitudeOfCompleteness : Position of the
+%           magnitude which is the magnitude of completeness
 
-b_value(1).Time(:,1) = b_value_time_points;
-
-b_value(1).Magnitude = CurrentWindowMagnitude;
-
-b_value(1).NofHitsInEachInterval = NofHitsInEachInterval;
-
-b_value(1).MagnitudeOfCompleteness = MagnitudeOfCompleteness;
-
-b_value(1).PositionOfMagnitudeOfCompleteness = PositionOfMagnitudeOfCompleteness;
-%   NOTE: "T" is a structure because originally this function was designed
-%   to work with a code where it was a structure, but for this function
-%   actually only a time array "T" is necessary.
-
-b_value = struct('Value',{},'Time',{},'Magnitude',{},'NofHitsInEachInterval',{},'MagnitudeOfCompleteness',{},'PositionOfMagnitudeOfCompleteness',{});
+args.MagRange = range(T.Magnitude);
 
 NumOfIntervals = floor(args.MagRange/args.bValueMagnitudeIntervalSize);
-
-bValueIntervals = linspace(min(args.Magnitude),max(args.Magnitude),NumOfIntervals);
-
-NofHitsInEachInterval = cell(1,NumOfIntervals);
-
+bValueIntervals(:,1) = ...
+    linspace(min(T.Magnitude),max(T.Magnitude),NumOfIntervals);
+NofHitsInEachInterval = cell(NumOfIntervals,1);
 CurrentWindowPosition = 1:args.bValueNumOfPointsInWindow;
-
 counter = 1;
+
+% Initialize variables
+CurrentWindowMagnitudes = cell(numel(T.Signal),1);
+bValueTimePoints = zeros(numel(T.Signal),1);
+NofHitsOfMagnitudeOfCompleteness = zeros(numel(T.Signal),1);
+PositionOfMagnitudeOfCompleteness = zeros(numel(T.Signal),1);
+MagnitudeOfCompleteness = zeros(numel(T.Signal),1);
 
 while(CurrentWindowPosition(end) <= numel(T.Time))
     NofHitsInEachInterval{counter} = zeros(2,NumOfIntervals);
 
     for iIntervals=1:NumOfIntervals
-        NofHitsInEachInterval{counter}(1,iIntervals) = bValueIntervals(iIntervals);
-
-        NofHitsInEachInterval{counter}(2,iIntervals) = nnz(args.Magnitude(CurrentWindowPosition) >= bValueIntervals(iIntervals));
+        NofHitsInEachInterval{counter}(1,iIntervals) = ...
+            bValueIntervals(iIntervals);
+        NofHitsInEachInterval{counter}(2,iIntervals) = ...
+            nnz(T.Magnitude(CurrentWindowPosition) >= bValueIntervals(iIntervals));
     end
 
-    CurrentWindowMagnitude{counter} = args.Magnitude(CurrentWindowPosition);
+    CurrentWindowMagnitudes{counter} = T.Magnitude(CurrentWindowPosition);
+    bValueTimePoints(counter) = mean(T.Time(CurrentWindowPosition));
+    
+    [NofHitsOfMagnitudeOfCompleteness(counter),...
+     PositionOfMagnitudeOfCompleteness(counter)] = ...
+        max(flip(NofHitsInEachInterval{counter}(2,:)));
 
-    b_value_time_points(counter) = mean(T.Time(CurrentWindowPosition));
-
-    [NofHitsOfMagnitudeOfCompleteness(counter),PositionOfMagnitudeOfCompleteness(counter)] = max(flip(NofHitsInEachInterval{counter}(2,:)));
-
-    PositionOfMagnitudeOfCompleteness(counter) = numel(NofHitsInEachInterval{counter}(2,:)) - PositionOfMagnitudeOfCompleteness(counter) + 1;
-
-    MagnitudeOfCompleteness(counter) = NofHitsInEachInterval{counter}(1,PositionOfMagnitudeOfCompleteness(counter));
-
-    CurrentWindowPosition = CurrentWindowPosition + args.bValueNumOfPointsToShiftInWindow;
-
+    PositionOfMagnitudeOfCompleteness(counter) = ...
+        numel(NofHitsInEachInterval{counter}(2,:)) - ...
+        PositionOfMagnitudeOfCompleteness(counter) + 1;
+    MagnitudeOfCompleteness(counter) = ...
+        NofHitsInEachInterval{counter}(1,PositionOfMagnitudeOfCompleteness(counter));
+    CurrentWindowPosition = ...
+        CurrentWindowPosition + args.bValueNumOfPointsToShiftInWindow;
     counter = counter+1;
 end
 
-RegressionLineCoefficients = zeros(numel(b_value_time_points),2);
+% Remove empty data
+NofHitsInEachInterval = ...
+    NofHitsInEachInterval(~cellfun(@isempty,NofHitsInEachInterval));
+bValueTimePoints(counter:end) = [];
+MagnitudeOfCompleteness(counter:end) = [];
+PositionOfMagnitudeOfCompleteness(counter:end) = [];
 
-X = cell(1,numel(b_value_time_points));
+RegressionLineCoefficients = zeros(numel(bValueTimePoints),2);
+X = cell(numel(bValueTimePoints),1);
+Y = cell(numel(bValueTimePoints),1);
+xl = cell(numel(bValueTimePoints),1);
+yl = cell(numel(bValueTimePoints),1);
 
-Y = cell(1,numel(b_value_time_points));
-
-yl = cell(1,numel(b_value_time_points));
-
-for iBvalue=1:numel(b_value_time_points)
+for iBvalue=1:numel(bValueTimePoints)
     Filt = NofHitsInEachInterval{iBvalue}(2,:) ~= 0;
-
     X{iBvalue} = NofHitsInEachInterval{iBvalue}(1,Filt);
-
     Y{iBvalue} = log10(NofHitsInEachInterval{iBvalue}(2,Filt));
-
-    RegressionLineCoefficients(iBvalue,:) = polyfix(X{iBvalue},Y{iBvalue},1,X{iBvalue}(PositionOfMagnitudeOfCompleteness(iBvalue)), ...
-        ...
-        Y{iBvalue}(PositionOfMagnitudeOfCompleteness(iBvalue)));
-
+    RegressionLineCoefficients(iBvalue,:) = ...
+        polyfix(X{iBvalue},...
+                Y{iBvalue},1,...
+                X{iBvalue}(PositionOfMagnitudeOfCompleteness(iBvalue)), ...
+                Y{iBvalue}(PositionOfMagnitudeOfCompleteness(iBvalue)));
     xl{iBvalue} = X{iBvalue}(PositionOfMagnitudeOfCompleteness(iBvalue):end);
-
-    yl{iBvalue} = RegressionLineCoefficients(iBvalue,1)*xl{iBvalue} + RegressionLineCoefficients(iBvalue,2);
+    yl{iBvalue} = RegressionLineCoefficients(iBvalue,1)*xl{iBvalue} + ...
+                  RegressionLineCoefficients(iBvalue,2);
 end
 
-b_value(1).Value(:,1) = abs(RegressionLineCoefficients(:,1));
-
-b_value(1).Time(:,1) = b_value_time_points;
-
-b_value(1).Magnitude = CurrentWindowMagnitude;
-
-b_value(1).NofHitsInEachInterval = NofHitsInEachInterval;
-
-b_value(1).MagnitudeOfCompleteness = MagnitudeOfCompleteness;
-
-b_value(1).PositionOfMagnitudeOfCompleteness = PositionOfMagnitudeOfCompleteness;
+b_value.Input.Time = T.Time;
+b_value.Input.Signal = T.Signal;
+b_value.Input.Magnitude = T.Magnitude;
+b_value.Output.Value(:,1) = abs(RegressionLineCoefficients(:,1));
+b_value.Output.Time(:,1) = bValueTimePoints;
+b_value.Aux.NofHitsInEachInterval = cellfun(@transpose,...
+                                           NofHitsInEachInterval,...
+                                           'UniformOutput', false);
+b_value.Aux.MagnitudeOfCompleteness = MagnitudeOfCompleteness;
+b_value.Aux.PositionOfMagnitudeOfCompleteness = ...
+    PositionOfMagnitudeOfCompleteness;
 end
---
+++ example.m
@@ -0,0 +1,111 @@
+clear
+close all
+clc
+%%
+% Dummy parameters to create a N-element signal with amplitude between
+% "a" and "b", with a final time of "tf"
+N = 2000;
+a = 0;
+b = 4;
+tf = 100; 
+
+T.Time = sort(0 + (tf-0).*rand(N,1));
+T.Signal = a + (b-a).*rand(N,1);
+
+% plot the dummy signal
+figure;
+stairs(T.Time,T.Signal);
+title("Original Signal");
+ylabel("Signal");
+xlabel("Time");
+
+% Define the reference magnitude. The magnitude in the code is measured in
+% dB, which is a relative scale. If the signal amplitude is measured in mV,
+% for example, the reference amplitude is in mV. It depends on the
+% characteristics of the equipment used for acquisition. 
+% If the reference magnitude is not known, at least set it to be equal to
+% the minimum signal amplitude, so that the magnitude will at least always 
+% be positive 
+
+% If known, for example
+% args.ReferenceMagnitude = 1e-3;
+% If unknown
+args.ReferenceMagnitude = min(T.Signal);
+
+T.Magnitude = log10(T.Signal/args.ReferenceMagnitude);
+
+% plot the magnitude
+figure;
+title("Magnitude of the signal")
+stairs(T.Time,T.Magnitude);
+ylabel("Magnitude [dB]");
+xlabel("Time");
+
+% Define b-value input arguments
+args.bValueNumOfPointsInWindow = floor(N/10);
+
+% To not utilize a sliding window set equal to 
+% "args.bValueNumOfPointsInWindow"
+
+% No sliding window
+% args.bValueNumOfPointsToShiftInWindow = bValueNumOfPointsInWindow;
+% Sliding window
+args.bValueNumOfPointsToShiftInWindow = floor(N/10);
+
+% To define the "bValueMagnitudeIntervalSize" it is important to take a
+% look of the magnitude range of the signals. For example, if the magnitude
+% levels vary between 0 and 1, a "bValueMagnitudeIntervalSize" of 0.1 could
+% be used. If it varies between 0 and 2.6, for example, a 
+% "bValueMagnitudeIntervalSize" of 0.5 could be used.
+% To decide, one helpful way is to plot a histogram of the magnitude and to
+% decide from there.
+
+% To help deciding the "bValueMagnitudeIntervalSize"
+figure;
+histogram(T.Magnitude);
+title("Histogram of the signal magnitude");
+ylabel("N of signals");
+xlabel("Magnitude [dB]");
+
+% After analysis of the magnitude range, for example
+% args.bValueMagnitudeIntervalSize = 0.05;
+% If unknown
+args.bValueMagnitudeIntervalSize = 0.1;
+
+% Run the code
+b_value = bValue(T,args);
+
+% Plot the output data (b_value and time)
+figure;
+plot(b_value.Output.Time,b_value.Output.Value);
+title("b-Value of original signal");
+ylabel("b-Value");
+xlabel("Time");
+
+% For analysis purposes, one can analyze the auxiliary data provided by the
+% "Aux" substructure. It contains, for each window: 
+% - The number of signals
+% - The Magnitude of completeness
+% - The position, in the bin, of the magnitude of completeness
+%% Example 2: Signal with increasing magnitudes
+% As the magnitude of signals increase, the b-value should decrease. To 
+% verify if that is the case, sort the signals in ascending order
+T.Magnitude = sort(T.Magnitude);
+b_value = bValue(T,args);
+
+figure;
+plot(b_value.Output.Time,b_value.Output.Value);
+title("b-Value of increasing-magnitude signal");
+ylabel("b-Value");
+xlabel("Time");
+%% Example 3: Signal with decreasing magnitudes
+% As the magnitude of signals decrease, the b-value should increase. To 
+% verify if that is the case, sort the signals in descending order
+T.Magnitude = sort(T.Magnitude,"descend");
+b_value = bValue(T,args);
+
+figure;
+plot(b_value.Output.Time,b_value.Output.Value);
+title("b-Value of decreasing-magnitude signal");
+ylabel("b-Value");
+xlabel("Time");
